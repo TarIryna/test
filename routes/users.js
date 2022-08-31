8ca45d34478c380dee484e26eac9ab025b4d7efd
TarIryna <tar86irina@gmail.com>
2022-08-31 11:08:51 +0300

    deleted commented lines

--- routes/users.js
+++ routes/users.js
@@ -12,16 +12,5 @@ router.post('/greeting', ageVerify, ctrlWrapper(displayName))
 router.post('/add', ageVerify, ctrlWrapper(addUser))
 router.get('/email', ctrlWrapper(findByMail))
 router.get('/fetch', ctrlWrapper(fetchData))
-// router.get('/fetch', async (req, res) => {
-//   const url = 'https://reqres.in/api/users'
-//   const options = {
-//     method: 'GET'
-//   }
-//   const response = await fetch(url, options)
-//     .then(res => res.json())
-//     .catch(e => console.log(e))
-//   console.log('RESPONSE: ', response)
-//   res.json(response)
-// })
 
 module.exports = router
